652cf0062ae6d792c092c84aee1813d67436e98d
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-17 11:27:17 -0400

    comments in page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,12 @@ import Image from "next/image";
 
 // localhost:3000
 
+// This may look like a regular react component,
+// But it's actually a Server-side rendered.
+// If you want it to be client-side, the file must start with:
+// "use client"
+// If you use useState, useEffect, or any other hooks dependant on the client, you must use 'use client' at the top
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
