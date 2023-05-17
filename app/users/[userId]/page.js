e32037223469f99c209a971161f3e9000fa3cb58
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-17 17:34:22 -0400

    2 fetch funcs written

--- app/users/[userId]/page.js
+++ app/users/[userId]/page.js
@@ -1,8 +1,19 @@
+import getSingleUser from "@/lib/getSingleUser";
 import React from "react";
 
 const SingleUser = ({ params }) => {
   console.log(params.userId);
-  return <div></div>;
+  const user = getSingleUser(params.userId);
+
+  const content = (
+    <div>
+      <h2 className="bold text-xl">Details on {user.username}</h2>
+      <p>Name: {user.name}</p>
+      <p>E-mail: {user.email}</p>
+      <p>Phone Number: {user.phone}</p>
+    </div>
+  );
+  return content;
 };
 
 export default SingleUser;
