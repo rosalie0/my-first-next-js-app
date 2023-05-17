de0c036396601549d372d91b9b75e62651fd5c43
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-17 17:47:38 -0400

    fetches single user and their posts in parallel.

--- app/users/[userId]/page.js
+++ app/users/[userId]/page.js
@@ -1,16 +1,30 @@
 import getSingleUser from "@/lib/getSingleUser";
+import getUserPosts from "@/lib/getUserPosts";
 import React from "react";
 
-const SingleUser = ({ params }) => {
+const SingleUser = async ({ params }) => {
   console.log(params.userId);
-  const user = getSingleUser(params.userId);
 
+  // Both of these are promises (they have not been awaited)
+  const userData = getSingleUser(params.userId); // don't await
+  const userPostsData = getUserPosts(params.userId);
+
+  // This resolves IN PARALLEL, not WATERFALL.
+  // We can fire both off at the same time and wait for both to finish (rather than one at a time)
+  const [user, userPosts] = await Promise.all([userData, userPostsData]);
   const content = (
     <div>
       <h2 className="bold text-xl">Details on {user.username}</h2>
       <p>Name: {user.name}</p>
       <p>E-mail: {user.email}</p>
       <p>Phone Number: {user.phone}</p>
+      <h3>Posts:</h3>
+      {userPosts.map((post) => (
+        <div>
+          <h4>{post.title}</h4>
+          <p>{post.body}</p>
+        </div>
+      ))}
     </div>
   );
   return content;
